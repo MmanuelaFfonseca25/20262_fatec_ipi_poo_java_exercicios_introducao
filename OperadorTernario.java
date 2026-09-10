4b857cbd9eb7850fd72dacbc86d3ad360e4ebeeb
Manuela Ramos Fonseca <manuelafonsecaramos@gmail.com>
2026-09-10 18:24:35 -0300

    feat(ap01-java-ex): implementa switche ternario

---
+++ OperadorTernario.java
@@ -0,0 +1,7 @@
+public class OperadorTernario{
+    public static void main (String[] args){
+        double nota =6;
+        //
+        System.out.println(nota>=6 ? "Aprovado" : "Reprovado" );
+    }
+}
